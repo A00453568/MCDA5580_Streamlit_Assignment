a31ace6e3d7b35d3668e5401b2bc354faf1e71b6
A00453568 <90723999+A00453568@users.noreply.github.com>
2022-04-14 02:51:20 -0300

    Update assignment6.py

--- assignment6.py
+++ assignment6.py
@@ -12,29 +12,29 @@ import streamlit as st
 st.title("Bitcoin Prices")
 
 currency_list = ['cad','usd','inr']
-day_list=[1,7,30,90,365]
-interval_list = ['daily','']
-
-currency='cad'
-day=90
 interval='daily'
 
-API_URL = '''https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=cad&days=90&interva
-l=daily'''
-BASE_URL='https://api.coingecko.com/api/v3/coins/bitcoin/market_chart'
+#API_URL=BASE_URL + '?vs_currency='+currency + '&days='+ str(day)+'&interval=' + interval
+API_URL='https://api.coingecko.com/api/v3/coins/bitcoin/market_chart'
 
-API_URL=BASE_URL + '?vs_currency='+currency + '&days='+ str(day)+'&interval=' + interval
-req = requests.get(API_URL)
+day = st.slider('No of Days', min_value=1, max_value=365)
+currency = st.radio('Currency', currency_list)
+
+payload = {'vs_currency':currency,'days':str(day),'interval':interval}
+req = requests.get(API_URL,payload)
 
 if(req.status_code==200):
     raw_data = req.json()
 
-df = pd.DataFrame(raw_data['prices'],
-                  columns=['date','price'])
+    df = pd.DataFrame(raw_data['prices'], columns=['date','price'])
+    df['date'] = pd.to_datetime(df['date'], unit='ms')
+    df.sort_values(by="date",inplace=True)
+    df = df.set_index('date')
+    st.line_chart(df)
+    
+    average_price = df['price'].mean()
+    result = "Average price during this time was "+ str(average_price)+' '+currency
+    st.write(result)
 
-df['date'] = pd.to_datetime(df['date'], unit='ms')
-df.sort_values(by="date",inplace=True)
-df.plot.line(x="date", y="price")
-average_price = df['price'].mean()
-result = "Average price during this time was "+ str(average_price)+' '+currency
-st.write(result)
+else:
+    st.write('API has failed to fetch data.')
